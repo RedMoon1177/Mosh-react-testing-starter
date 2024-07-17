6945c77bd940ae79f13d39f6035d70b72927e570
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-07-17 16:20:42 -0300

    completed 6 - Testing Lists

--- src/components/UserAccount.tsx
+++ src/components/UserAccount.tsx
@@ -13,3 +13,12 @@ const UserAccount = ({ user }: { user: User }) => {
 };
 
 export default UserAccount;
+
+
+// Syntax lesson learnt:
+// ({ user }: { user: User }):
+
+// This part is the TypeScript type annotation for the function parameter. 
+// It uses destructuring to extract the user property from the passed object.
+// The parameter is annotated with { user: User }, 
+// which means the function expects an object with a user property of type User.
