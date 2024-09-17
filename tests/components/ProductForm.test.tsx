db6c1e21e80702a392bb59eb603cab2882485d74
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-09-16 22:23:14 -0300

    completed Testing Form - 9, 10

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,13 +1,9 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ProductForm from "../../src/components/ProductForm";
 import AllProviders from "../AllProviders";
 import { Category, Product } from "../../src/entities";
 import { db } from "../mocks/db";
-import { input } from "@testing-library/user-event/dist/cjs/event/input.js";
+import userEvent from "@testing-library/user-event";
 
 describe("ProductForm", () => {
   let category: Category;
@@ -26,12 +22,55 @@ describe("ProductForm", () => {
     });
 
     return {
+      expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
+        const error = screen.getByRole("alert");
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveTextContent(errorMessage);
+      },
+
       waitForFormToLoad: async () => {
         await screen.findByRole("form");
+        const nameInput = screen.getByPlaceholderText(/name/i);
+        const priceInput = screen.getByPlaceholderText(/price/i);
+        const categoryInput = screen.getByRole("combobox", {
+          name: /category/i,
+        });
+        const submitButton = screen.getByRole("button");
+
+        type FormData = {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          [K in keyof Product]: any;
+        };
+
+        const validData: FormData = {
+          id: 1,
+          name: "a",
+          price: 1,
+          categoryId: 1,
+        };
+
+        const fill = async (product: FormData) => {
+          const user = userEvent.setup();
+
+          if (product.name !== undefined)
+            await user.type(nameInput, product.name);
+
+          if (product.price !== undefined)
+            await user.type(priceInput, product.price.toString());
+
+          await user.click(categoryInput);
+          const options = screen.getAllByRole("option");
+          await user.click(options[0]);
+          await user.click(submitButton);
+        };
+
         return {
-          nameInput: screen.getByPlaceholderText(/name/i),
-          priceInput: screen.getByPlaceholderText(/price/i),
-          categoryInput: screen.getByRole("combobox", { name: /category/i }),
+          nameInput,
+          priceInput,
+          categoryInput,
+          submitButton,
+          fill,
+          validData,
         };
       },
     };
@@ -66,4 +105,65 @@ describe("ProductForm", () => {
     const { nameInput } = await waitForFormToLoad();
     expect(nameInput).toHaveFocus();
   });
+
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "longer than 255 characters",
+      name: "a".repeat(256),
+      errorMessage: /255/i,
+    },
+  ])(
+    "should display an error if name is $scenario",
+    async ({ name, errorMessage }) => {
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
+
+      const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, name });
+
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
+
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /1/i,
+    },
+    {
+      scenario: "negative",
+      price: -1,
+      errorMessage: /1/i,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/i,
+    },
+    {
+      scenario: "not a number",
+      price: "a",
+      errorMessage: /required/i,
+    },
+  ])(
+    "should display an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad, expectErrorToBeInTheDocument } =
+        renderComponent();
+
+      const form = await waitForFormToLoad();
+      await form.fill({ ...form.validData, price });
+
+      expectErrorToBeInTheDocument(errorMessage);
+    }
+  );
 });
