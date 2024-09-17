130691c27f2db5e7c47a24226b61579800eecaf2
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-09-16 21:49:30 -0300

    Completed Testing Form - 5, 6

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -7,6 +7,7 @@ import ProductForm from "../../src/components/ProductForm";
 import AllProviders from "../AllProviders";
 import { Category, Product } from "../../src/entities";
 import { db } from "../mocks/db";
+import { input } from "@testing-library/user-event/dist/cjs/event/input.js";
 
 describe("ProductForm", () => {
   let category: Category;
@@ -19,17 +20,31 @@ describe("ProductForm", () => {
     db.category.delete({ where: { id: { equals: category.id } } });
   });
 
+  const renderComponent = (product?: Product) => {
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, {
+      wrapper: AllProviders,
+    });
+
+    return {
+      waitForFormToLoad: async () => {
+        await screen.findByRole("form");
+        return {
+          nameInput: screen.getByPlaceholderText(/name/i),
+          priceInput: screen.getByPlaceholderText(/price/i),
+          categoryInput: screen.getByRole("combobox", { name: /category/i }),
+        };
+      },
+    };
+  };
+
   it("should render form fields", async () => {
-    render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders });
-    // 1st approach:
-    // await screen.findByRole("form");
-    // 2nd approach:
-    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
-    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
-    expect(
-      screen.getByRole("combobox", { name: /category/i })
-    ).toBeInTheDocument();
+    const { waitForFormToLoad } = renderComponent();
+
+    const { nameInput, priceInput, categoryInput } = await waitForFormToLoad();
+
+    expect(nameInput).toBeInTheDocument();
+    expect(priceInput).toBeInTheDocument();
+    expect(categoryInput).toBeInTheDocument();
   });
 
   it("should populate form fields when editing a product", async () => {
@@ -39,16 +54,16 @@ describe("ProductForm", () => {
       price: 10,
       categoryId: category.id,
     };
-    render(<ProductForm product={product} onSubmit={vi.fn()} />, {
-      wrapper: AllProviders,
-    });
-    await screen.findByRole("form");
-    expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
-    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
-      product.price.toString()
-    );
-    expect(
-      screen.getByRole("combobox", { name: /category/i })
-    ).toHaveTextContent(category.name);
+    const { waitForFormToLoad } = renderComponent(product);
+    const inputs = await waitForFormToLoad();
+    expect(inputs.nameInput).toHaveValue(product.name);
+    expect(inputs.priceInput).toHaveValue(product.price.toString());
+    expect(inputs.categoryInput).toHaveTextContent(category.name);
+  });
+
+  it("should put focus on the name field", async () => {
+    const { waitForFormToLoad } = renderComponent();
+    const { nameInput } = await waitForFormToLoad();
+    expect(nameInput).toHaveFocus();
   });
 });
